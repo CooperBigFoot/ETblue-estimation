2229ce1efc728eaba8ce986d68f0650745a75821
“CooperBigFoot” <nlazaro@student.ethz.ch>
2024-10-03 22:27:53 +0200

    export decadal downscaled WaPOR et to assets

--- utils/asset_management.py
+++ utils/asset_management.py
@@ -1,14 +1,15 @@
 import ee
 
+
 def delete_folder_contents(folder_path):
     # Initialize the Earth Engine API
-    ee.Initialize(projct='thurgau-irrigation')
+    ee.Initialize(project="thurgau-irrigation")
 
     # List all assets in the specified folder
-    assets = ee.data.listAssets({'parent': folder_path})
+    assets = ee.data.listAssets({"parent": folder_path})
 
     # Extract the asset IDs
-    asset_ids = [asset['id'] for asset in assets['assets']]
+    asset_ids = [asset["id"] for asset in assets["assets"]]
 
     # Delete each asset
     for asset_id in asset_ids:
@@ -18,7 +19,10 @@ def delete_folder_contents(folder_path):
         except ee.EEException as e:
             print(f"Error deleting {asset_id}: {str(e)}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Example usage
-    folder_path = 'projects/thurgau-irrigation/assets/Thurgau/ET_WaPOR_downscaled_dekadal'
+    folder_path = (
+        "projects/thurgau-irrigation/assets/Thurgau/ET_WaPOR_10m_dekadal_2018"
+    )
     delete_folder_contents(folder_path)
