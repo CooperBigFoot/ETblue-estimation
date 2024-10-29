0c421e5c209a8d0f1d16f85e44f00fdf46706d85
“CooperBigFoot” <nlazaro@student.ethz.ch>
2024-10-29 14:11:16 +0100

    Export ET_blue m3/ha/year from wapor monthly and dekadal for Thurgau

--- utils/asset_management.py
+++ utils/asset_management.py
@@ -62,7 +62,7 @@ def keep_first_n_images(collection_id: str, n: int) -> str:
 
 if __name__ == "__main__":
 
-    collection_id = "projects/thurgau-irrigation/assets/Zuerich/ET_green_wapor_jurisdiction_zh_dekadal_2022"
+    collection_id = "projects/thurgau-irrigation/assets/Thurgau/ET_blue_postprocessed_2018-2022"
     delete_folder_contents(collection_id)
 
     # # Example usage of keep_first_n_images
